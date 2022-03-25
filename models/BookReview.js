22b2184ba65392496afc77f4092418b2df2e372e
lconnell1000 <lconnell1000@gmail.com>
2022-03-25 15:49:20 +1030

    adding book working

--- models/BookReview.js
+++ models/BookReview.js
@@ -11,6 +11,19 @@ BookReview.init(
       primaryKey: true,
       autoIncrement: true,
     },
+    review_text: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    star_rating: {
+        type: DataTypes.DECIMAL,
+    },
+    review_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
+    
     user_id: {
         type: DataTypes.INTEGER,
         references: {
@@ -24,14 +37,6 @@ BookReview.init(
             model: 'book',
             key: 'id',
         }
-    },
-    star_rating: {
-        type: DataTypes.DECIMAL,
-    },
-    review_date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
     },
   },
   {
