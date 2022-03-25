8adc2f978a633160ffca1e7a91e9a49e0eeb2e2c
Clay <claymcintosh@gmail.com>
2022-03-25 10:57:28 +1000

    Added multer npm and got file uploads working. Currently working in server.js and will need to be moved

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Book, User } = require('../models');
 const withAuth = require('../utils/auth');
 
+
 router.get('/', async (req, res) => {
   try {
     // Get all books and JOIN with user data
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-//const formidable = require('formidable');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -26,6 +25,33 @@ const sess = {
 };
 
 app.use(session(sess));
+
+//START CLAY TEST
+
+const multer = require('multer');
+
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './public/uploads')
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "--" + file.originalname);
+  }
+})
+
+const upload = multer({ storage: fileStorageEngine })
+
+app.post("/single", upload.single("image"), (req, res) => {
+  console.log(req.file);
+  res.send("Single file upload success");
+});
+
+//END CLAY TEST
+
+
+
+
+
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
