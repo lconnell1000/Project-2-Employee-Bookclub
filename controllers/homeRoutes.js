d1204c1db7f7457b701eaf68c72f3dd73ba24ca3
Clay <claymcintosh@gmail.com>
2022-03-26 20:12:33 +1000

    have basic card info in library.handlebars.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -96,10 +96,23 @@ router.get('/books', (req, res) => {
   }
 });
 
-router.get('/library', (req, res) => {
-  if (req.session.logged_in) {
-    res.render('library');
-    return;
+
+router.get('/library', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Book }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('library', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
