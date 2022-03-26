4765dfad099c1636dc7dee7287a36432dac63c09
Clay <claymcintosh@gmail.com>
2022-03-26 16:33:50 +1000

    changed update in api routes

--- public/js/cover.js
+++ public/js/cover.js
@@ -1,28 +1,49 @@
-const newFormHandler = async (event) => {
-    event.preventDefault();
+// // const newFormHandler = async (event) => {
+// //     event.preventDefault();
   
-    const title = document.querySelector('#book-title').value.trim();
-    const author = document.querySelector('#book-author').value.trim();
-    const genre = document.querySelector('#book-genre').value.trim();
+// //     const title = document.querySelector('#book-title').value.trim();
+    
+// //     if (title && author && genre) {
+// //       const response = await fetch(`/api/book`, {
+// //         method: 'POST',
+// //         body: JSON.stringify({ title, author, genre }),
+// //         headers: {
+// //           'Content-Type': 'application/json',
+// //         },
+// //       });
   
-    if (title && author && genre) {
-      const response = await fetch(`/api/book`, {
-        method: 'POST',
-        body: JSON.stringify({ title, author, genre }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/cover');
-      } else {
-        alert('Failed to create book');
-      }
-    }
-  };
+// //       if (response.ok) {
+// //         document.location.replace('/cover');
+// //       } else {
+// //         alert('Failed to create book');
+// //       }
+// //     }
+// //   };
   
   
-  document
-    .querySelector('.new-book-form')
-    .addEventListener('submit', newFormHandler);
+// //   document
+// //     .querySelector('.new-book-form')
+// //     .addEventListener('submit', newFormHandler);
+
+// const delButtonHandler = async (event) => {
+//     if (event.target.hasAttribute('data-id')) {
+//       const id = event.target.getAttribute('data-id');
+  
+//       const response = await fetch(`/api/book/${id}`, {
+//         method: 'DELETE',
+//       });
+  
+//       if (response.ok) {
+//         document.location.replace('/profile');
+//       } else {
+//         alert('Failed to delete book');
+//       }
+//     }
+//   };
+  
+  
+//   document
+//     .querySelector('.book-list')
+//     .addEventListener('click', delButtonHandler);
+  
+  
