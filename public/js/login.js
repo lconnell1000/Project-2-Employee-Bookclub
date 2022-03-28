33720281dad23d2dd1115e5b42d45463681cdb11
lconnell1000 <lconnell1000@gmail.com>
2022-03-28 12:31:51 +1030

    fixed signup to be working

--- public/js/login.js
+++ public/js/login.js
@@ -25,15 +25,15 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const firstName = document.querySelector('#firstName-signup').value.trim();
-  const lastName = document.querySelector('#lastName-signup').value.trim();
+  const first_name = document.querySelector('#firstName-signup').value.trim();
+  const last_name = document.querySelector('#lastName-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  if (firstName && lastName && email && password) {
+  if (first_name && last_name && email && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ firstName, lastName, email, password }),
+      body: JSON.stringify({ first_name, last_name, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
